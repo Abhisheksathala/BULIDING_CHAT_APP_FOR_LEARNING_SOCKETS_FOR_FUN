79bec4ba964c9967ef15e44e4bcf7f2b6fc016a8
abhishek <abhisheksathala296@gmail.com>
2026-01-10 10:26:04 +0530

    Ad

--- client/src/components/dialogs/AddMemberDialog.jsx
+++ client/src/components/dialogs/AddMemberDialog.jsx
@@ -1,42 +1,53 @@
-import React from 'react'
-import {Button, Dialog, DialogTitle, Stack, Typography} from "@mui/material"
-import {userChatData} from "../constants/sampleData.js";
-import UserItem from "../shared/UserItem.jsx";
+import React from 'react';
+import { Button, Dialog, DialogTitle, Stack, Typography } from '@mui/material';
+import { userChatData } from '../constants/sampleData.js';
+import UserItem from '../shared/UserItem.jsx';
 
-const AddMemberDialog = ({addMember, isLoadingAddMemer, ChatId}) => {
+const AddMemberDialog = ({ addMember, isLoadingAddMemer, ChatId }) => {
 
-    const addFriendHandler = () => {
+    const [members, setMembers] = React.useState(userChatData);
+    const [selectedMembers, setSelectedMembers] = React.useState([]);
 
-    }
-    const addFrienSubmitdHandler = () => {
+    const selectMemberhandler = (id) => {
+      // setMembers((prev) =>
+      //   prev.map((user) => (user._id === id ? { ...user, isAdded: !user.isAdded } : user)),
+      // );
+      setSelectedMembers((prev) =>
+        prev.includes(id) ? prev.filter((currentElemt) => currentElemt !== id) : [...prev, id],
+      );
+    };
 
-    }
-    const closeHandler = () => {
+  const addFriendHandler = () => {};
+  const addFrienSubmitdHandler = () => {};
+  const closeHandler = () => {};
 
-    }
-
-    return (
-
-        <Dialog open onClose={closeHandler}>
-            <Stack p={"2rem"} width={"20rem"} spacing={"2rem"}>
-                <DialogTitle textAlign={"center"}>Add Member</DialogTitle>
-                <Stack>
-                    {
-                        userChatData.length > 0 ? userChatData.map((i) => {
-                            return (
-                                <UserItem user={i} handler={addFriendHandler}/>
-                            )
-                        }) : <Typography textAlign={"center"}>No Friends </Typography>
-                    }
-                </Stack>
-                <Stack direction={"row"} alignItems={"center"} justifyContent={"space-evenly"}>
-                    <Button color={"error"} onClick={() => closeHandler()}>Cancle</Button>
-                    <Button onClick={() => addFrienSubmitdHandler()} variant={"contained"} disabled={isLoadingAddMemer}>Submit
-                        Changes</Button>
-                </Stack>
-            </Stack>
-        </Dialog>
-
-    )
-}
-export default AddMemberDialog
+  return (
+    <Dialog open onClose={closeHandler}>
+      <Stack p={'2rem'} width={'20rem'} spacing={'2rem'}>
+        <DialogTitle textAlign={'center'}>Add Member</DialogTitle>
+        <Stack>
+          {userChatData.length > 0 ? (
+            userChatData.map((i) => {
+              return <UserItem user={i} isAdded={selectedMembers} handler={selectMemberhandler} />;
+            })
+          ) : (
+            <Typography textAlign={'center'}>No Friends </Typography>
+          )}
+        </Stack>
+        <Stack direction={'row'} alignItems={'center'} justifyContent={'space-evenly'}>
+          <Button color={'error'} onClick={() => closeHandler()}>
+            Cancle
+          </Button>
+          <Button
+            onClick={() => addFrienSubmitdHandler()}
+            variant={'contained'}
+            disabled={isLoadingAddMemer}
+          >
+            Submit Changes
+          </Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
+};
+export default AddMemberDialog;
