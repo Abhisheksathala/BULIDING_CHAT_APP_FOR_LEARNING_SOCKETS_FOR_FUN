c347838b6c977db4ba7181cc28d1b880c14955b2
abhishek <abhisheksathala296@gmail.com>
2026-01-15 17:43:19 +0530

    add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,57 +1,77 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from 'react-router-dom';
-import ProtectRoute from './components/auth/ProtectRoute';
-import LayoutLoader from './components/layout/Loader';
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
+import axios from "axios"
 
-// admin pages 
+
+// loader
+import LayoutLoader from "./components/layout/Loader";
+
+// admin pages
 import AdminLogin from "./pages/admin/AdminLogin.jsx";
 import Dashboard from "./pages/admin/Dashboard.jsx";
-const   ChatManagement = React.lazy(()=> import("../src/pages/admin/ChatManagement.jsx"))
-const   Usermanagement = React.lazy(()=> import("../src/pages/admin/Usermanagement.jsx"))
-const   Groupmanagement = React.lazy(()=> import("../src/pages/admin/Groupmanagment.jsx"))
-const Message = React.lazy(()=> import("../src/pages/admin/Message.jsx"))
+const ChatManagement = React.lazy(() =>
+  import("../src/pages/admin/ChatManagement.jsx")
+);
+const Usermanagement = React.lazy(() =>
+  import("../src/pages/admin/Usermanagement.jsx")
+);
+const Groupmanagement = React.lazy(() =>
+  import("../src/pages/admin/Groupmanagment.jsx")
+);
+const Message = React.lazy(() => import("../src/pages/admin/Message.jsx"));
 
 //user  PAGES
-const Home = React.lazy(() => import('./pages/Home'));
-const Login = React.lazy(() => import('./pages/Login'));
-const Chat = React.lazy(() => import('./pages/Chat'));
-const Groups = React.lazy(() => import('./pages/Groups'));
-const NotFound = React.lazy(() => import('./pages/PagenotFound'));
+const Home = React.lazy(() => import("./pages/Home"));
+const Login = React.lazy(() => import("./pages/Login"));
+const Chat = React.lazy(() => import("./pages/Chat"));
+const Groups = React.lazy(() => import("./pages/Groups"));
+const NotFound = React.lazy(() => import("./pages/PagenotFound"));
+
+// env imports 
+import { server } from "./constants/config.js";
 
 let user = true;
 
-const App = () => {
-    return (
-        <Suspense
-            fallback={
-                <>
-                    <LayoutLoader/>
-                </>
-            }
-        >
-            <Routes>
-                <Route element={<ProtectRoute user={user}/>}>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/chat/:id" element={<Chat/>}/>
-                    <Route path="/groups" element={<Groups/>}/>
-                </Route>
-                <Route element={<ProtectRoute user={!user} redirect="/"/>}>
-                    <Route path="/userauthentication">
-                        <Route path="login" element={<Login type={'login'}/>}/>
-                        <Route path="register" element={<Login type={'signin'}/>}/>
-                    </Route>
-                </Route>
 
-                <Route path="/admin" element={<AdminLogin/>}/>
-                <Route path="/admin/dashboard" element={<Dashboard/>}/>
-                <Route path="/admin/ChatManagement" element={<ChatManagement/>}/>
-                <Route path="/admin/user-managment" element={<Usermanagement/>}/>
-                <Route path="/admin/group-management" element={<Groupmanagement/>}/>
-                <Route path="/admin/messages" element={<Message/>}/>
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
-        </Suspense>
-    );
+const App = () => {
+    React.useEffect(()=>{
+    // console.log(server)
+    axios.get(`${server}/api`)
+    },[])
+  return (
+    <Suspense
+      fallback={
+        <>
+          <LayoutLoader />
+        </>
+      }
+    >
+      <Routes>
+        {/*  */}
+        <Route element={<ProtectRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:id" element={<Chat />} />
+          <Route path="/groups" element={<Groups />} />
+        </Route>
+        {/*  */}
+        <Route element={<ProtectRoute user={!user} redirect="/" />}>
+          <Route path="/userauthentication">
+            <Route path="login" element={<Login type={"login"} />} />
+            <Route path="register" element={<Login type={"signin"} />} />
+          </Route>
+        </Route>
+        {/*  */}
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/ChatManagement" element={<ChatManagement />} />
+        <Route path="/admin/user-managment" element={<Usermanagement />} />
+        <Route path="/admin/group-management" element={<Groupmanagement />} />
+        <Route path="/admin/messages" element={<Message />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,19 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Suspense } from 'react';
+import {Provider} from "react-redux"
+import store from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <HelmetProvider>
-        <div onContextMenu={(e) => e.preventDefault()}></div>
+        <div onContextMenu={(e) => e.preventDefault()}>
         <App />
+        </div>
       </HelmetProvider>
     </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
